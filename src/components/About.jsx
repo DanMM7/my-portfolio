5076994c61ed5b29e0c899231025925217962a5a
Dan Malengela <93791798+DanMM7@users.noreply.github.com>
2023-12-21 16:22:32 -0600

    Changed personal summary

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        Focused and quick-learning Software Developer with 3 years of experience
+        in data analysis, data visualization, computer science, programming,
+        and UX design for various projects and clients.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
