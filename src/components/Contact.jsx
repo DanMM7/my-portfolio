34b58753a6842cb1663afc906b172b75f1f0eb2e
Dan Malengela <93791798+DanMM7@users.noreply.github.com>
2024-01-06 16:38:28 -0600

    Fixed email bug

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,11 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_my2plgb',
+        'template_7bmah7d',
+
+        //import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        //import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Dan Malengela",
@@ -42,7 +45,8 @@ const Contact = () => {
           to_email: "dmmalengela@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'JB7LfNc43gIEC-hah'
+        //import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
